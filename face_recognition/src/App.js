deabdd270b4aec3621f6e394e3f941f535e57f96
Yamini-Namburi <34385539+Yamini-Namburi@users.noreply.github.com>
2018-02-22 16:22:27 +0000

    change done in the register.js

--- face_recognition/src/App.js
+++ face_recognition/src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -74,8 +75,13 @@ displayFaceBox = (box) => {
   }
 
   onRouteChange = (route) => {
-    this.setState({route: route});
+    if (route === 'signout') {
+    this.setState({isSignedIn: false});
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true})
   }
+  this.setState({route: route});
+}
 
   render() {
     return (
@@ -84,10 +90,9 @@ displayFaceBox = (box) => {
               params={particlesOptions}
                
             />
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home'
-       ?  
-       <div> 
+       ?  <div> 
        <Logo />
        <Rank />
         < ImageLinkForm 
